fa6942b7690dd96602256502f87da12c0d3a473c
DMHacker123 <matosalvarezdm@hotmail.com>
2026-07-21 19:33:44 -0400

    Add temporary debug route

--- cse-341-graphql-books/server.js
+++ cse-341-graphql-books/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const { createHandler } = require('graphql-http/lib/use/express');
 const mongodb = require('./db/connect');
 const { schema, root } = require('./schema/schema');
@@ -10,8 +12,21 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.all('/graphql', createHandler({ schema, rootValue: root }));
+
+app.get('/debug', (req, res) => {
+  const cwd = process.cwd();
+  const dirname = __dirname;
+  let publicContents;
+  try {
+    publicContents = fs.readdirSync(path.join(__dirname, 'public'));
+  } catch (err) {
+    publicContents = `ERROR: ${err.message}`;
+  }
+  res.json({ cwd, dirname, publicContents });
+});
 
 app.get('/', (req, res) => {
   res.send('GraphQL Books API is running! Visit /graphql to query.');
